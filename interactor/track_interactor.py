f13452c7a2ac9891258b7e5da117481c694d5306
ooyamatakehisa <ooyamatakehisa@gmail.com>
2021-10-15 03:28:59 +0900

    implement get_track_by_ids method(#7)

--- interactor/track_interactor.py
+++ interactor/track_interactor.py
@@ -20,7 +20,66 @@ class TrackInteractor(TrackUsecase):
         self.access_token_usecase = access_token_usecase
         self.logger = logger
 
-    def get_tracks(self, query: str) -> list:
+    def get_features_by_ids(self, ids: list, access_token: str = None):
+        if access_token is None:
+            access_token = self.access_token_usecase.get_access_token()
+
+        response = requests.get(
+            "https://api.spotify.com/v1/audio-features",
+            headers={"Authorization": f"Bearer {access_token}"},
+            params={
+                "ids": ",".join(ids),
+            },
+        )
+
+        if response.status_code != requests.codes.ok:
+            self.logger.error(
+                f"cannot fetch search result features correctly: {response.json()}"
+            )
+
+        features = response.json()["audio_features"]
+        return features
+
+    def get_tracks_by_ids(self, ids: list) -> list:
+        self.logger.error(ids)
+        access_token = self.access_token_usecase.get_access_token()
+
+        response = requests.get(
+            "https://api.spotify.com/v1/tracks",
+            headers={"Authorization": f"Bearer {access_token}"},
+            params={
+                "ids": ",".join(ids),
+            },
+        )
+
+        if response.status_code != requests.codes.ok:
+            self.logger.error(
+                f"cannot fetch tracks by ids correctly: {response.json()}"
+            )
+
+        items = response.json()["tracks"]
+        features = self.get_features_by_ids(ids)
+
+        tracks = [
+            {
+                "spotify_id": item["id"],
+                "song_name": item["name"],
+                "album_name": item["album"]["name"],
+                "artist": item["artists"][0]["name"],
+                "bpm": features[idx]["tempo"],
+                "key": features[idx]["key"],
+                "mode": features[idx]["mode"],
+                "image_url": item["album"]["images"][0]["url"],
+                "preview_url": item["preview_url"],
+                "danceability": features[idx]["danceability"],
+                "energy": features[idx]["energy"],
+            }
+            for idx, item in enumerate(items)
+        ]
+
+        return tracks
+
+    def get_tracks_by_query(self, query: str) -> list:
         access_token = self.access_token_usecase.get_access_token()
 
         response = requests.get(
@@ -42,42 +101,24 @@ class TrackInteractor(TrackUsecase):
             self.logger.info("no search result for the specified query.")
             return []
 
-        response = requests.get(
-            "https://api.spotify.com/v1/audio-features",
-            headers={"Authorization": f"Bearer {access_token}"},
-            params={
-                "ids": ",".join(map(lambda item: item["id"], items)),
-            },
-        )
+        ids = map(lambda item: item["id"], items)
+        features = self.get_features_by_ids(ids)
 
-        if response.status_code != requests.codes.ok:
-            self.logger.error(
-                f"cannot fetch search result features correctly: {response.json()}"
-            )
-
-        features = response.json()["audio_features"]
-
-        tracks = []
-        for idx, item in enumerate(items):
-            spotify_id = item["id"]
-            song_name = item["name"]
-            artist = item["artists"][0]["name"]
-            image_url = item["album"]["images"][0]["url"]
-            album_name = item["album"]["name"]
-            preview_url = item["preview_url"]
-
-            tracks.append({
-                "spotify_id": spotify_id,
-                "song_name": song_name,
-                "album_name": album_name,
-                "artist": artist,
+        tracks = [
+            {
+                "spotify_id": item["id"],
+                "song_name": item["name"],
+                "album_name": item["album"]["name"],
+                "artist": item["artists"][0]["name"],
                 "bpm": features[idx]["tempo"],
                 "key": features[idx]["key"],
                 "mode": features[idx]["mode"],
-                "image_url": image_url,
-                "preview_url": preview_url,
+                "image_url": item["album"]["images"][0]["url"],
+                "preview_url": item["preview_url"],
                 "danceability": features[idx]["danceability"],
                 "energy": features[idx]["energy"],
-            })
+            }
+            for idx, item in enumerate(items)
+        ]
 
         return tracks
--- interface/usecase/track_usecase.py
+++ interface/usecase/track_usecase.py
@@ -3,5 +3,9 @@ from abc import ABCMeta, abstractmethod
 
 class TrackUsecase(metaclass=ABCMeta):
     @abstractmethod
-    def get_tracks(self, query: str) -> list:
+    def get_tracks_by_query(self, query: str) -> list:
+        pass
+
+    @abstractmethod
+    def get_tracks_by_ids(self, ids: list) -> list:
         pass
