0897b37d6f4353d78d65a1d38bdb3e4e3d77bd13
ooyamatakehisa <ooyamatakehisa@gmail.com>
2022-12-12 01:45:11 +0900

    change number of workers to 1

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 flask db migrate
 flask db upgrade
-gunicorn --bind=0.0.0.0:8080 --workers=16 --access-logfile - app:app
+gunicorn --bind=0.0.0.0:8080 --workers=1 --access-logfile - app:app
